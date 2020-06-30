dc8ef6450701cbda9f0c6e534eaae6215077c17b
Jose Esteves <jose@ceibo.co>
2020-06-30 16:19:16 -0300

    first commit

---
+++ lib/phx_admin_lte.ex
@@ -0,0 +1,18 @@
+defmodule PhxAdminLte do
+  @moduledoc """
+  Documentation for PhxAdminLte.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhxAdminLte.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phx_admin_lte_test.exs
@@ -0,0 +1,8 @@
+defmodule PhxAdminLteTest do
+  use ExUnit.Case
+  doctest PhxAdminLte
+
+  test "greets the world" do
+    assert PhxAdminLte.hello() == :world
+  end
+end
